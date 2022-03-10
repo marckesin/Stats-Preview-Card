c45805d2c056463b3aa549a99d2d7068b8163240
marckesin <192.168.2.1@protonmail.com>
2022-03-09 23:21:09 -0300

    Configuração inicial

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Footer from "../components/footer";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+// Home component to render the landing page
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Frontend Mentor | Stats preview card component</title>
+        <meta name="description" content="Stats preview card component." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}></main>
+      <Footer />
+    </div>
+  );
+}
